f624a805e0df40a4d3587662630b1278bd909bed
Derek Meyers <p.derekmeyers@gmail.com>
2025-06-12 17:27:03 -0400

    Added some spacers, another button, applied modifiers to an HStack, etc.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,19 +14,32 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
+                .fontWeight(.ultraLight)
                 .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
             
-            Button("Click Me!") {
-                message = "Awesome!"
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                Button("Great") {
+                    message = "Great!"
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
             // To clear the console: command + K
         }
         .padding()
